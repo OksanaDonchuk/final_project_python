b25537e5b9ca4bfe565ef0c8b02efedb5f9a6f67
Oksana Donchuk <80112131+OksanaDonchuk@users.noreply.github.com>
2024-08-15 20:12:25 +0300

    Shaw email address (#10)
    * add show_email
    
    * Add show address and email

--- my_contacts_book/my_contacts_book/main.py
+++ my_contacts_book/my_contacts_book/main.py
@@ -4,7 +4,7 @@ from address_book import AddressBook
 # from notes import Notes
 from handlers import (
     add_contact, change_birthday, change_contact, change_name, delete_contact, show_phone, show_all,
-    add_birthday, show_birthday, birthdays, add_email, delete_email, change_email, add_address, delete_address, change_address, show_contact)
+    add_birthday, show_birthday, show_email, show_address, birthdays, add_email, delete_email, change_email, add_address, delete_address, change_address, show_contact)
 
 
 from colorama import init, Fore, Style
@@ -105,12 +105,16 @@ def handle_action(action: str, args: list[str], book: AddressBook) -> str:
             return add_birthday(args, book)
         case "show-birthday":
             return show_birthday(args, book)
+        case "show_address":
+            return show_address(args, book)
         case "birthdays":
             return birthdays(args, book)
         case "change-birthday":
             return change_birthday(args, book)
         case "add-email":
             return add_email(args, book)
+        case "show_email":
+            return show_email(args, book)
         case "delete-email":
             return delete_email(args, book)
         case "change-email":
@@ -180,11 +184,13 @@ def print_help() -> str:
     - birthdays: Shows upcoming birthdays within the next 7 days.
     - change-birthday <name> <new_birthday>: Changes the birthday for an existing contact.
     - add-email <name> <email>: Add an email to the specified contact.
+    - show_email <name>:  Shows the email for the specified contact.
     - delete-email <name> <email>: Delete an email to the specified contact.
     - change-email <name> <new email>: Change an email to the specified contact.
     - add-address <name> <address>: Adds an address to the specified contact.
     - delete-address <name>: Delete an address to the specified contact.
     - change-address <name> <new address>: Change an address to the specified contact.
+    - show_address <name>:  Shows the address for the specified contact.
     - delete <name>: Deletes a contact from the address book.
     - add-note
     - change-note
@@ -209,7 +215,8 @@ def main() -> None:
 
         action, args = parse_input(user_input)
 
-        COMMANDS = ["hello", "add", "change-name", "change", "phone", "all", "add-birthday", "show-birthday", "birthdays", "change-birthday", "add-email", "delete-email", "change-email", "add-address", "delete-address", "change-address", "delete", "add-note", "change-note", "delete-note", "show-all-notes", "find-note-by-title", "find-note-by-tag", "help", "close", "exit", "bye", "contact"]
+        COMMANDS = ["hello", "add", "change-name", "change", "phone", "all", "add-birthday", "show-birthday",  "birthdays", "change-birthday", "show_email", "show_address", "add-email", "delete-email", "change-email", "add-address", "delete-address", "change-address", "delete", "add-note", "change-note", "delete-note", "show-all-notes", "find-note-by-title", "find-note-by-tag", "help", "close", "exit", "bye", "contact"]
+        
         suggested_command = suggest_command(action, COMMANDS)
 
         if suggested_command and suggested_command != action:
